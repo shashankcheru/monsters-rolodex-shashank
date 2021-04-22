604be48692748ce6cc8941084e49aa941df092e2
Shashank C <shashank.c@valtech.com>
2021-04-22 19:20:54 +0530

    adding files for gihub pages

---
+++ src/components/search-box/search-box.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './search-box.css';
+
+export const SearchBox = ({ placeholder, handleChange}) => {
+    return (
+        <div>
+            <input className='search' type='search' placeholder={placeholder} onChange={handleChange}/>
+        </div>
+    );
+}
